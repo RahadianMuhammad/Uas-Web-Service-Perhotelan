ca7bb01d1852d6a1c38c17a18ad0c3e14eddff23
RahadianMuhammad <rahadian1506@gmail.com>
2022-08-01 22:45:03 +0800

    instal jwt dan crud user api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+
+
+Route::group(['middleware' => ['auth:api']], function () {
+
+    Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 });
